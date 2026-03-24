7839d84c860b6f180789d8c53772ec2d698eda14
Victor Eloi <victorhugoeloi@gmail.com>
2026-03-24 16:22:18 -0300

    First Tests

---
+++ src/test/java/core/Constants.java
@@ -0,0 +1,15 @@
+package core;
+
+
+import io.restassured.http.ContentType;
+
+public interface Constants {
+
+    String BASE_URL = "https://barrigarest.wcaquino.me";
+    Integer PORT = 443; //http-80
+    String BASE_PATH = "";
+
+    ContentType CONTENT_TYPE = ContentType.JSON;
+
+    Long MAX_TIMEOUT = 5000L;
+}
---
+++ src/test/java/core/RequestBase.java
@@ -0,0 +1,32 @@
+package core;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
+
+public class RequestBase implements Constants{
+
+    @BeforeClass
+    public static void setup(){
+        RestAssured.baseURI = BASE_URL;
+        RestAssured.port = PORT;
+        RestAssured.basePath = BASE_PATH;
+
+        RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+        reqBuilder.setContentType(CONTENT_TYPE);
+        reqBuilder.log(LogDetail.ALL);
+        RestAssured.requestSpecification = reqBuilder.build();
+
+        ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+        resBuilder.expectResponseTime(Matchers.lessThan(MAX_TIMEOUT));
+        resBuilder.log(LogDetail.ALL);
+        RestAssured.responseSpecification = resBuilder.build();
+
+        // RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+    }
+
+}
